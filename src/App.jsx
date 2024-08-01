92a3a8c31174dac2d9187326f65bd17f8f796763
ErikaCZanin <erikaczbu@gmail.com>
2024-08-01 19:17:33 -0300

    atualizando conteudo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+<div className='container'>
+<div className='containernaveg'>
+<h1>Teste</h1>
+</div>
+<div className='contanerdash'>
+  <h1>teste</h1>
+</div>
+<div className='containerstatic'>
+  <h1>teste</h1>
+</div>
+</div>
+)
+}
+
+export default App
