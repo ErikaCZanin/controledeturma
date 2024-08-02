7bddca50b18d9e47b5fb5313d350a52a339f5a92
ErikaCZanin <erikaczbu@gmail.com>
2024-08-01 21:02:44 -0300

    atualização css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import './App.css'
 
+const alunos = [
+  { foto: 'https://tse2.explicit.bing.net/th?id=OIP.LW0fGX7DpufHVYoHRes4LgHaE8&pid=Api&P=0&h=180', nome: 'Aluno 1' },
+  { foto: 'https://tse2.explicit.bing.net/th?id=OIP.LW0fGX7DpufHVYoHRes4LgHaE8&pid=Api&P=0&h=180', nome: 'Aluno 1' },
+  { foto: 'https://tse2.explicit.bing.net/th?id=OIP.LW0fGX7DpufHVYoHRes4LgHaE8&pid=Api&P=0&h=180', nome: 'Aluno 1' },
+  { foto: 'https://tse2.explicit.bing.net/th?id=OIP.LW0fGX7DpufHVYoHRes4LgHaE8&pid=Api&P=0&h=180', nome: 'Aluno 1' },
+
+
+
+]
+
 function App() {
   return (
-<div className='container'>
-<div className='containernaveg'>
-<h1>Teste</h1>
-</div>
-<div className='contanerdash'>
-  <h1>teste</h1>
-</div>
-<div className='containerstatic'>
-  <h1>teste</h1>
-</div>
-</div>
-)
+    <div className='container'>
+      <div className='containernaveg'>
+        <h1>Menu</h1>
+      </div>
+      <div className='contanerdash'>
+        <div className='listalunos'>
+          <h1 className='titulo'>Alunos</h1>
+          <div className='alunos-list'>
+            {alunos.map((aluno, index) => (
+              <div key={index} className='aluno-item'>
+                <img src={aluno.foto} alt={aluno.nome} className='aluno-foto' />
+                <p className='aluno-nome'>{aluno.nome}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className='containerstatic'>
+        <h1>Grafico</h1>
+      </div>
+    </div>
+  )
 }
 
 export default App
